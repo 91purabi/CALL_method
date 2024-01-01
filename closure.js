c35a9aaa2b27d6b44b524740e22b8e5faca307c5
Purabi <purbichoudhury06966@gmail.com>
2024-01-01 23:42:04 +0530

    let var const practice

--- closure.js
+++ closure.js
@@ -9,16 +9,64 @@
 // var closure = outer();
 // closure();
 
-function outerFunction(x){
-    function innerFunction(y){
-        return x + y;
+// function outerFunction(x){
+//     function innerFunction(y){
+//         return x + y;
+//     }
+
+//     return innerFunction;
+// }
+
+// const closureExample = outerFunction(10);
+
+// console.log(closureExample(5));
+// console.log(closureExample(8));
+// console.log(closureExample(10));
+
+
+// function createCounter(){
+//     let count = 0;
+
+//     return {
+//         increment: function(){
+//             count++;
+//         },
+//         getCount: function(){
+//             return count;
+//         }
+//     };
+// }
+
+// const counter = createCounter();
+// counter.increment();
+// console.log(counter.getCount());
+
+
+// function multiplier(factor){
+//     return function(x){
+//         return x * factor;
+//     };
+// }
+
+// const double = multiplier(2);
+// console.log(double(5));
+
+function exampleVar(){
+    if(true){
+        var x = 10;
     }
 
-    return innerFunction;
+    console.log(x);
 }
 
-const closureExample = outerFunction(10);
+function exampleLetConst(){
+    if(true){
+        let y = 20;
+        const z = 30;
+    }
+    console.log(y); //y is not defined
+}
 
-console.log(closureExample(5));
-console.log(closureExample(8));
-console.log(closureExample(10));
+exampleVar();  // 10
+exampleLetConst();
+
