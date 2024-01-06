9d26c7ce0771f248e1b84f803e91511664890058
Purabi <purbichoudhury06966@gmail.com>
2024-01-07 01:41:07 +0530

    variables

---
+++ letContVar.js
@@ -0,0 +1,22 @@
+// function exampleVar(){
+//     if(true){
+//         var x = 10;
+//     }
+
+//     console.log(x);
+// }
+
+// exampleVar();
+
+
+function exampleLet(){
+    if(true){
+        let y = 20;
+        console.log(y);
+    }
+
+    let y = 30;
+    console.log(y);
+}
+
+exampleLet();
