c761ca396e17c5f1222350d27b0a7eb61cc37fa1
Purabi <purbichoudhury06966@gmail.com>
2024-01-07 06:29:11 +0530

    const

--- letContVar.js
+++ letContVar.js
@@ -9,14 +9,24 @@
 // exampleVar();
 
 
-function exampleLet(){
-    if(true){
-        let y = 20;
-        console.log(y);
-    }
+// function exampleLet(){
+//     if(true){
+//         let y = 20;
+//         console.log(y);
+//     }
 
-    let y = 30;
-    console.log(y);
+//     let y = 30;
+//     console.log(y);
+// }
+
+// exampleLet();
+
+
+function exampleConst(){
+    if(true){
+        const z = 40;
+        console.log(z);
+    }
 }
 
-exampleLet();
+exampleConst();
