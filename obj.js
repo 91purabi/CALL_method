1160e17fd4b802203904c3a5305bf382d4da7c11
Purabi <purbichoudhury06966@gmail.com>
2024-01-17 23:03:32 +0530

    obj

---
+++ obj.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "banana", "orange"];
+console.log(fruits[1]);  
+let person = {
+    firstName: "Alice",
+    lastName: "Johnson",
+    age: 30
+};
+
+console.log(person.firstName);  
